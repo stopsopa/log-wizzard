c55deb913caa67fc868200dda165a75007c9a126
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2023-09-16 21:45:42 +0100

    showdown from shell script

---
+++ .github/clean_before_artifact.sh
@@ -0,0 +1,22 @@
+
+
+_SHELL="$(ps "${$}" | grep "${$} " | grep -v grep | sed -rn "s/.*[-\/]+(bash|z?sh) .*/\1/p")"; # bash || sh || zsh
+case ${_SHELL} in
+  zsh)
+    _DIR="$( cd "$( dirname "${(%):-%N}" )" && pwd -P )"
+    ;;
+  sh)
+    _DIR="$( cd "$( dirname "${0}" )" && pwd -P )"
+    ;;
+  *)
+    _DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+    ;;
+esac
+
+ROOT="${_DIR}/.."
+
+cd "${ROOT}"
+
+set -x
+rm -rf node_modules 
+rm -rf package.json
---
+++ .github/showdown.sh
@@ -0,0 +1,28 @@
+
+
+_SHELL="$(ps "${$}" | grep "${$} " | grep -v grep | sed -rn "s/.*[-\/]+(bash|z?sh) .*/\1/p")"; # bash || sh || zsh
+case ${_SHELL} in
+  zsh)
+    _DIR="$( cd "$( dirname "${(%):-%N}" )" && pwd -P )"
+    ;;
+  sh)
+    _DIR="$( cd "$( dirname "${0}" )" && pwd -P )"
+    ;;
+  *)
+    _DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+    ;;
+esac
+
+ROOT="${_DIR}/.."
+
+cd "${ROOT}"
+
+set -e
+set -x
+
+echo "{}" > package.json
+
+yarn add showdown
+
+cat README.md | node node_modules/.bin/showdown makehtml > index.html
+
